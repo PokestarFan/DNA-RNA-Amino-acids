43d84f0fb117fbf447bcaa6d0e1dc84458ae5d46
PokestarFan <azoysarkar@gmail.com>
2018-01-25 16:08:59 -0500

    Add changes

--- program.py
+++ program.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# lisence
+# Copyright 2018 PokestarFan
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#tkinter setup
+try:
+    import tkinter as tk  # for python 3
+except:
+    import Tkinter as tk  # for python 2
+
+#get dicts
+import dicts
+
+#conversion
 def dna_to_rna(dna):
     newstring = ''
     dna = dna.upper()
@@ -7,3 +26,41 @@ def dna_to_rna(dna):
         else:
             newstring += i
     return newstring
+
+def rna_to_dna(rna):
+    newstring = ''
+    rna = rna.upper()
+    for i in rna:
+        if i == 'U':
+            newstring += 'T'
+        else:
+            newstring += i
+    return newstring
+
+
+#opposites
+def opposite_rna(rna):
+    codons = []
+    count = 0
+    adder = ''
+    amino_acids = ''
+    rna = rna.upper()
+    if 'T' in rna:
+        rna = dna_to_rna(dna)
+    for i in rna:
+        count = 1
+        added += i
+        if count == 3:
+            codons.append(added)
+            count = 0
+            adder = ''
+    for codon in codons:
+        assert len(codon) == 3
+        amino_acid = dicts.amino_acids[codon].upper()[0:3]
+        if amino_acid == 'STO':
+            amino_acid = 'STOP'
+        amino_acids += amino_acid
+    return amino_acids
+
+
+#the tkinter
